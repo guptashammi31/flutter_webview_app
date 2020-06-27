6845d3788c3eda14fa7c44231a630ee97eb21f71
guptashammi31 <guptashammi31@gmail.com>
2020-06-27 13:40:33 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/technogenr/flutterwebviewapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.technogenr.flutterwebviewapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/webview_container.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewContainer extends StatefulWidget {
+  final url;
+  final title;
+  WebViewContainer(this.url, this.title);
+  @override
+  createState() => _WebViewContainerState(this.url, this.title);
+}
+
+class _WebViewContainerState extends State<WebViewContainer> {
+  var _url;
+  var _title;
+  final _key = UniqueKey();
+  _WebViewContainerState(this._url, this._title);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        Image.asset(
+        'assets/logo.jpeg',
+        fit: BoxFit.contain,
+        height: 32,
+    ),
+    Container(
+    padding: const EdgeInsets.all(8.0), child: Text(_title))
+    ],
+
+    ),),
+      body: Column(
+        children: [
+          Expanded(
+            child: WebView(
+              key: _key,
+              javascriptMode: JavascriptMode.unrestricted,
+              initialUrl: _url,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
